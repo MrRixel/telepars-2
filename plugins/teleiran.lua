a2b78a347e7346788d08c641231957999815cf65
Armin <armin.bagheri.1380@gmail.com>
2016-01-28 23:24:46 +0330

    Create teleiran.lua

---
+++ plugins/teleiran.lua
@@ -0,0 +1,17 @@
+do
+
+function run(msg, matches)
+  return " The Invition Link for \n TeleIran support : \n https://telegram.me/joinchat/BrnPkDwgJWt8EK5YBqDksA"
+  end
+return {
+  description = "shows teleiran link", 
+  usage = "!teleiran : Return supports link",
+  patterns = {
+    "^teleiran$",
+    "^/teleiran$",
+    "^!teleiran$",
+    "^>teleiran$",
+  },
+  run = run
+}
+end
