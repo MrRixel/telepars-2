9bf686bdb053d44d402a61d48f2513a6beeabaf8
Armin <armin.bagheri.1380@gmail.com>
2016-01-29 00:29:24 +0330

    Update slm.lua

--- plugins/slm.lua
+++ plugins/slm.lua
@@ -1,12 +1,12 @@
 do
 
 function run(msg, matches)
-  return " سلام " .. matches[0]
+  return " سلام " .. matches[1]
 end
 
 return {
     patterns = {
-    " سلام "
+    "سلام"
   }, 
   run = run 
 }
